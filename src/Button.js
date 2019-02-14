24946237daa3fc695d5ca587bfaaff2b10cddc4e
Martin Harrigan <martinharrigan@gmail.com>
2019-02-14 09:40:50 +0100

    Added a new button component

---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button>This is my button</button>
+    );
+  }
+}
+
+export default Button;

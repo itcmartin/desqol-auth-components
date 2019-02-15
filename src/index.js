735dce7fba1ba3fd15cb521ae4f8a3a727eb1029
modybutt <45882806+modybutt@users.noreply.github.com>
2019-02-15 11:06:36 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,47 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import WiredRegistration from './WiredRegistration';
+import WiredLogin from './WiredLogin';
+import Logout from './Logout';
+import Profile from './Profile';
 
-import Registration from '../Registration'
-import Login from '../Login'
-import Profile from '../Profile'
-import Logout from '../Logout'
-import EmailInput from '../EmailInput'
-import PasswordInput from '../PasswordInput'
-import DisplayNameInput from '../DisplayNameInput'
-import Pineapple from '../Pineapple'
+import './styles.css';
 
-storiesOf('Registration', module).add('default', () =>
-  <Registration onRegister={action('register')} />);
+function App() {
+  return (
+    <div className='App'>
+      <WiredRegistration
+        onSuccess={e => alert(JSON.stringify(e))}
+        onFailure={e => alert(JSON.stringify(e))}
+        onError={e => alert(JSON.stringify(e))}
+      />
+      <hr />
+      <WiredLogin
+        onSuccess={e => alert(JSON.stringify(e))}
+        onFailure={e => alert(JSON.stringify(e))}
+        onError={e => alert(JSON.stringify(e))}
+      />
+      <hr />
+      <Profile />
+      <hr />
+      <Logout />
+    </div>
+  );
+}
 
-storiesOf('Login', module).add('default', () =>
-  <Login onLogin={action('login')} />);
-
-storiesOf('Profile', module).add('default', () =>
-  <Profile onUpdate={action('update')} />);
-
-storiesOf('Logout', module).add('default', () =>
-  <Logout onLogout={action('logout')} />);
-
-storiesOf('EmailInput', module).add('default', () =>
-  <EmailInput onChange={action('change')} />);
-
-storiesOf('PasswordInput', module)
-  .add('with confirmation', () =>
-    <PasswordInput showConfirm={true} onChange={action('change')} />)
-  .add('without confirmation', () =>
-    <PasswordInput showConfirm={false} onChange={action('change')} />)
-
-storiesOf('DisplayNameInput', module).add('default', () =>
-  <DisplayNameInput onChange={action('change')} />);
-
-storiesOf('Pineapple', module)
-	.add('01', () => <Pineapple  />)
-	.add('02', () => <Pineapple emotion="02" />)
-	.add('03', () => <Pineapple emotion="03" />)
-	.add('04', () => <Pineapple emotion="04" />)
-	.add('05', () => <Pineapple emotion="06" />)
-	.add('06', () => <Pineapple emotion="07" />)
-	
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
